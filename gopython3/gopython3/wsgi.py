667e8b0904cbcd4be8172c03b7b8dd73bed6ac16
Ilya Baryshev <baryshev@gmail.com>
2013-09-28 09:30:37 +0400

    Django 1.6 + django-configurations

---
+++ gopython3/gopython3/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for gopython3 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en//howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gopython3.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+
+from configurations.wsgi import get_wsgi_application
+application = get_wsgi_application()
