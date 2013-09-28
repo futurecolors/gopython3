55d377c57f720752619daf1fdf36d1213e7eaea9
melevir <melevir@gmail.com>
2013-09-28 13:49:42 +0400

    Extracting required info from PyPI

--- gopython3/api/concrete_wrappers.py
+++ gopython3/api/concrete_wrappers.py
@@ -1,5 +1,7 @@
-from . import abstract_wrappers
 from django.conf import settings
+import requests
+
+from . import abstract_wrappers
 
 
 class PyPIWrapper(abstract_wrappers.AbstractJsonApiWrapper):
@@ -11,6 +13,27 @@ class PyPIWrapper(abstract_wrappers.AbstractJsonApiWrapper):
             self.hammock = self.hammock(version)
         self.hammock = self.hammock.json
         return 'GET', {}
+
+    def get_short_info(self, name, version=None):
+        name = self.get_correct_name(name)
+        data = self.ask_about_package_info(name=name, version=version)
+        return {
+            'last_release_date': data['urls'][0]['upload_time'],
+            'python3_supported_versions': list(self.get_py3_supported_versions(data)),
+            'url': data['info']['package_url'],
+            'name': name,
+            'version': data['info']['version']
+        }
+
+    def get_py3_supported_versions(self, data):
+        language_classifiers = data['info']['classifiers']
+        python_info = filter(lambda c: c.startswith('Programming Language') and 'Python' in c, language_classifiers)
+        py3_versions = filter(lambda v: v[0] == '3', [v.split('::')[-1].strip() for v in python_info])
+        return py3_versions
+
+    def get_correct_name(self, name):
+        response = requests.get('http://pypi.python.org/simple/%s/' % name)
+        return response.url.split('/')[-2]
 
 
 class GithubWrapper(abstract_wrappers.AbstractJsonApiWrapperWithAuth):
