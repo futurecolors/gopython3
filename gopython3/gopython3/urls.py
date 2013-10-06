e58f727cfa5dc324f3779422f9e4ac567b36fb9c
coagulant <baryshev@gmail.com>
2013-10-06 11:33:04 +0400

    Favicon

--- gopython3/gopython3/urls.py
+++ gopython3/gopython3/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -7,7 +8,14 @@ from core.rest import router, PackageView
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns('')
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^(favicon.ico)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+
+urlpatterns += patterns('',
     url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/packages/(?P<code>.+)/$', PackageView.as_view(), name='jobspec-detail'),
     url(r'^api/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
