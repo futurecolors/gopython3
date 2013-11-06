166ec0487acf702db6e575bb4984e9a6c12544c3
coagulant <baryshev@gmail.com>
2013-11-06 08:47:02 +0400

    Added python3, improved updates

--- etc/install/bootstrap.sh
+++ etc/install/bootstrap.sh
@@ -2,6 +2,7 @@
 
 # Config
 PROJECT_NAME=gopython3
+USER=futurecolors
 
 CODENAME=precise
 PGSQL_VERSION=9.3
@@ -10,33 +11,37 @@ PROJECT_DIR=/home/vagrant/$PROJECT_NAME
 
 # Update package list
 apt-get update -y
-# Python dev packages
-apt-get install -y build-essential python python-dev
+apt-get install -y python-software-properties
+add-apt-repository -y ppa:fkrull/deadsnakes  # python
+add-apt-repository -y ppa:chris-lea/node.js  # node
+apt-get update -y
+# Python
+apt-get install -y build-essential python3.3 python python-dev
 # Dependencies for image processing
 apt-get install -y imagemagick libjpeg62-dev zlib1g-dev libfreetype6-dev liblcms1-dev
 # Git (that's how we deploy)
 apt-get install -y git
+# Nodejs
+apt-get install -y nodejs
 
+# our alterego
+adduser --home /home/$USER --disabled-password futurecolors --gecos ""
+
+# pip
+su -l futurecolors
+wget --quiet -O - https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python3.3
+pip install virtualenvwrapper
 
 # Postgresql PGSQL_VERSION
 if ! command -v psql; then
     echo "deb http://apt.postgresql.org/pub/repos/apt/ $CODENAME-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
-    wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+    wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
     apt-get update -y
     apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
-    cp $PROJECT_DIR/etc/install/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
+    cp /vagrant/etc/install/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
     /etc/init.d/postgresql reload
 fi
 
-
-# Node.js
-# http://stackoverflow.com/questions/16302436/install-nodejs-on-ubuntu-12-10
-if ! command -v npm; then
-    apt-get install -y python-software-properties g++ make
-    add-apt-repository ppa:chris-lea/node.js
-    apt-get update -y
-    apt-get install -y nodejs
-fi
 # Less
 npm config set loglevel error
 npm install -g less
